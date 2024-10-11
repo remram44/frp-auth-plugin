9b4fdaaf6a749abdda3a48f2d7e39d09cf9de3a2
Remi Rampin <remi@rampin.org>
2024-10-11 17:24:59 -0400

    Implement auth

--- frp-auth-plugin.go
+++ frp-auth-plugin.go
@@ -13,7 +13,7 @@ import (
 	"github.com/remram44/frp-auth-plugin/internal/configfile"
 )
 
-var config *configfile.Config
+var configFile *configfile.ConfigFile
 
 func main() {
 	ctx := context.Background()
@@ -23,10 +23,11 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Usage: frp-auth-plugin <configuration-file>\n")
 		os.Exit(2)
 	}
-	configFile := os.Args[1]
+	configFileName := os.Args[1]
 
 	// Read configuration
-	config, err := configfile.New(configFile)
+	var err error
+	configFile, err = configfile.New(configFileName, ctx)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading config file: %s\n", err)
 		os.Exit(1)
@@ -52,8 +53,6 @@ func main() {
 	if !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
-
-	_ = config
 }
 
 type FrpLogin struct {
@@ -68,8 +67,13 @@ type FrpLoginRequest struct {
 	Content FrpLogin `json:"content"`
 }
 
-type FrpNewProxy struct {
+type FrpNewProxyUser struct {
 	User  string            `json:"user"`
+	Metas map[string]string `json:"metas"`
+}
+
+type FrpNewProxy struct {
+	User  FrpNewProxyUser   `json:"user"`
 	Metas map[string]string `json:"metas"`
 
 	ProxyName string `json:"proxy_name"`
@@ -78,7 +82,9 @@ type FrpNewProxy struct {
 	RemotePort int `json:"remote_port"`
 
 	CustomDomains []string `json:"custom_domains"`
-	Subdomain     string   `json:"subdomain"`
+
+	HttpUser     string `json:"http_user,omitempty"`
+	HttpPassword string `json:"http_pwd,omitempty"`
 }
 
 type FrpNewProxyRequest struct {
@@ -89,6 +95,13 @@ func handleReq(res http.ResponseWriter, req *http.Request) {
 	// Read query parameters
 	query := req.URL.Query()
 	op := query.Get("op")
+
+	reject := func(reason string) {
+		res.WriteHeader(200)
+		io.WriteString(res, "{\"reject\": true, \"reject_reason\": \"")
+		io.WriteString(res, reason)
+		io.WriteString(res, "\"}")
+	}
 
 	// Read body
 	decoder := json.NewDecoder(req.Body)
@@ -107,8 +120,28 @@ func handleReq(res http.ResponseWriter, req *http.Request) {
 			http.Error(res, "Bad JSON", 400)
 			return
 		}
-		// TODO
-		log.Printf("Login: %s %s %s", body.Content.ClientAddress, body.Content.User, body.Content.Metas)
+
+		// Lookup user
+		config := configFile.CurrentConfig()
+		for _, user := range config.Users {
+			if user.Username != body.Content.User {
+				continue
+			}
+
+			if user.Password != body.Content.Metas["token"] {
+				log.Printf("Invalid password for %s", body.Content.User)
+				reject("invalid password")
+				return
+			}
+
+			log.Printf("Login from %s as %s", body.Content.ClientAddress, body.Content.User)
+			res.WriteHeader(200)
+			io.WriteString(res, "{\"reject\": false, \"unchange\": true}")
+			return
+		}
+
+		log.Printf("Invalid user %#v", body.Content.User)
+		reject("invalid user")
 	case "NewProxy":
 		var body FrpNewProxyRequest
 		err := decoder.Decode(&body)
@@ -117,7 +150,51 @@ func handleReq(res http.ResponseWriter, req *http.Request) {
 			http.Error(res, "Bad JSON", 400)
 			return
 		}
-		// TODO
+
+		// Lookup user
+		config := configFile.CurrentConfig()
+		for _, user := range config.Users {
+			if user.Username != body.Content.User.User {
+				continue
+			}
+
+			// Lookup proxy
+			for _, proxy := range user.Proxies {
+				if proxy.Name != body.Content.ProxyName {
+					continue
+				}
+
+				if body.Content.ProxyType != "http" {
+					reject("proxy not http")
+					return
+				}
+
+				// Replace proxy config with our own
+				newProxy := FrpNewProxy{
+					User:          body.Content.User,
+					Metas:         body.Content.Metas,
+					ProxyName:     body.Content.ProxyName,
+					ProxyType:     body.Content.ProxyType,
+					CustomDomains: proxy.CustomDomains,
+					HttpUser:      proxy.HttpUser,
+					HttpPassword:  proxy.HttpPassword,
+				}
+
+				log.Printf("NewProxy %s from %s", body.Content.ProxyName, body.Content.User.User)
+				res.WriteHeader(200)
+				io.WriteString(res, "{\"reject\": false, \"unchange\": false, \"content\":")
+				encoder := json.NewEncoder(res)
+				encoder.Encode(newProxy)
+				io.WriteString(res, "}")
+				return
+			}
+
+			log.Printf("Invalid proxy %s %s", body.Content.User, body.Content.ProxyName)
+			return
+		}
+
+		log.Printf("Invalid user %#v", body.Content.User)
+		reject("invalid user")
 	default:
 		res.WriteHeader(200)
 		io.WriteString(res, "{\"reject\": false, \"unchange\": true}")
--- internal/configfile/config.go
+++ internal/configfile/config.go
@@ -1,9 +1,105 @@
 package configfile
 
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+)
+
 type Config struct {
+	Users []User `json:"users"`
 }
 
-func New(file string) (*Config, error) {
-	config := &Config{}
-	return config, nil
+type User struct {
+	Username string  `json:"username"`
+	Password string  `json:"password"`
+	Proxies  []Proxy `json:"proxies"`
+}
+
+type Proxy struct {
+	Name          string   `json:"name"`
+	CustomDomains []string `json:"custom_domains"`
+	HttpUser      string   `json:"http_user"`
+	HttpPassword  string   `json:"http_password"`
+}
+
+type ConfigFile struct {
+	lastModified time.Time
+	config       *Config
+}
+
+func New(file string, ctx context.Context) (*ConfigFile, error) {
+	// Do first load
+	fileInfo, err := os.Stat(file)
+	if err != nil {
+		return nil, err
+	}
+	lastModified := fileInfo.ModTime()
+
+	fp, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	decoder := json.NewDecoder(fp)
+	decoder.DisallowUnknownFields()
+	var config Config
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	configFile := &ConfigFile{
+		lastModified: lastModified,
+		config:       &config,
+	}
+
+	// Reload file automatically in the background
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(1 * time.Minute):
+			}
+
+			fileInfo, err := os.Stat(file)
+			if err != nil {
+				log.Printf("Can't stat config file: %s", err)
+				continue
+			}
+
+			if fileInfo.ModTime() == configFile.lastModified {
+				continue
+			}
+
+			fp, err := os.Open(file)
+			if err != nil {
+				log.Printf("Can't open config file: %s", err)
+				continue
+			}
+			decoder := json.NewDecoder(fp)
+			decoder.DisallowUnknownFields()
+			var newConfig Config
+			err = decoder.Decode(&newConfig)
+			if err != nil {
+				log.Printf("Can't read config file: %s", err)
+				continue
+			}
+
+			log.Print("New config loaded")
+			encoder := json.NewEncoder(log.Writer())
+			encoder.SetIndent("", "")
+			encoder.Encode(newConfig)
+
+			configFile.config = &newConfig
+		}
+	}()
+
+	return configFile, nil
+}
+
+func (cf *ConfigFile) CurrentConfig() *Config {
+	return cf.config
 }
